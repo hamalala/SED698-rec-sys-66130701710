29ff5a9c2e236a932db6605689c37a493a655289
hamalala <46687725+hamalala@users.noreply.github.com>
2024-10-27 14:38:13 +0700

    Add files via upload

---
+++ app_rec_movie_66130701710.py
@@ -0,0 +1,18 @@
+
+# Load data back from the file
+with open('66130701710_recommendation_usersim.pkl', 'rb') as file:
+    user_similarity_df, user_movie_ratings = pickle.load(file)
+    
+st.title("Movie Recommendation System")
+
+# Input: Collect user_id from the user
+user_id = st.text_input("Enter User ID:", "")
+
+if user_id:
+    # Get recommendations (top 10)
+    recommendations = get_movie_recommendations(user_id, user_similarity_df, user_movie_ratings, 10)
+    
+    # Display recommendations
+    st.write(f"Top 10 movie recommendations for User {user_id}:")
+    for movie_title in recommendations:
+        st.write("• " + movie_title)
